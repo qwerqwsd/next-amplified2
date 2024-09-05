93f4945bef411b5f992b9dcb141397cd05a6123f
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 <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>
2024-09-05 09:27:25 +0900

    ass

---
+++ src/app/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [user, setUser] = useState(null);
+
+  function listTodos() {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      // 사용자가 로그인할 때마다 Todo 목록을 새로 로드합니다.
+      listTodos();
+    }
+  }, [user]);
+
+  function createTodo() {
+    client.models.Todo.create({
+      content: window.prompt("Todo content"),
+    });
+  }
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
+  return (
+    <>
+      <Authenticator>
+        {({ signOut, user }) => {
+
+          return (
+            <main>
+              <h1>{user?.signInDetails?.loginId}'s todos</h1>
+
+              <button onClick={createTodo}>+ new</button>
+              <ul>
+                {todos.map((todo) => (
+                  <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
+                ))}
+              </ul>
+              <div>
+                🥳 App successfully hosted. Try creating a new todo.
+                <br />
+                <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+                  Review next steps of this tutorial.
+                </a>
+              </div>
+              <button onClick={() => { signOut()}}>Sign out</button>
+            </main>
+          );
+        }}
+      </Authenticator>
+    </>
+  );
+}
